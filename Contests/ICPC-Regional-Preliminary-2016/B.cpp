7bc07f40ac09214323f7b35baec651778772221a
immuntasir <immuntasir@gmail.com>
2016-10-24 10:27:12 +0600

    Create B.cpp

---
+++ Contests/ICPC-Regional-Preliminary-2016/B.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main () {
+    printf("%d\n", 104);
+    for (int i=1; i<=100; i++) printf("%d ", 10);
+    printf("2 8 7 9\n");
+    return 0;
+}
